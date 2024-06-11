3deea43980ed56356d5a0015d043d32938618af9
almoghaviv <almoghaviv@gmail.com>
2024-06-11 10:04:08 +0300

    creating and testing the CPBC report to its fullest

--- src/CPBC_all.py
+++ src/CPBC_all.py
@@ -1,16 +1,25 @@
 import pandas as pd
 import os
 import re
-import openpyxl
-from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Font, Border, Side
+import numpy as np
+from openpyxl import load_workbook
+import shutil
+import datetime
 
 
 def main():
     input_path = os.path.join('..', 'data', 'jira_data.xlsx')
     output_path = ""
-    df = create_df_for_cpbc(input_path)
-    create_reports_from_data(df, output_path)
+
+    # Creates a dict of algo, bi, dev, devops and product with relevant df for each one of them.
+    dict_of_dfs = create_df_for_cpbc(input_path)
+
+    for department, df in dict_of_dfs.items():
+        print(f"{department} report is in process")
+        create_full_scale_for_excel(department, df)
+        print(f"{department} report is ready")
+
+    print("All the reports are ready")
     return
 
 
@@ -25,12 +34,7 @@ def create_df_for_cpbc(path):
 
         df_by_departments = departments_df(df)
 
-        print(df_by_departments)
-
-        filtered_df = formation_of_df_for_excel(df)
-
-        print(filtered_df)
-        return filtered_df
+        return df_by_departments
 
     except Exception as e:
         print(f"Error reading Excel file: {e}")
@@ -40,9 +44,13 @@ def create_df_for_cpbc(path):
 def departments_df(df: object):
     # Construct the relative path to the CSV file
     csv_file_path = os.path.join('..', 'config', 'worker-names.csv')
+    xlsx_file_path = os.path.join('..', 'config', 'fte_contract.csv')
 
     # Read the CSV file
     worker_config = pd.read_csv(csv_file_path)
+
+    # Read the CSV file
+    fte_contract = pd.read_csv(xlsx_file_path)
 
     # Create a mapping from assignee names to their corresponding columns in df1
     name_to_column = {}
@@ -61,6 +69,10 @@ def departments_df(df: object):
 
     # Loop through each category column in df1
     for category in worker_config.columns:
+        # get planned per month
+        fte_col = fte_contract[['month', category]]
+        fte_col['month'] = pd.to_datetime(fte_col['month'])
+        print(fte_col)
         # Get the list of assignees for the current category
         assignees = worker_config[category].dropna().tolist()
         # Filter df2 based on the assignees list
@@ -76,56 +88,129 @@ def departments_df(df: object):
         pivot_df = pivot_df.fillna(0)
         # Reset index to convert the pivoted DataFrame back to a normal DataFrame
         pivot_df = pivot_df.reset_index()
-        # Print the resulting DataFrame
-        print(pivot_df)
+        # Calculate the sum across the specified columns for each row
+        pivot_df['Total Time Spent'] = pivot_df.iloc[:, 2:].sum(axis=1)
+        # Let's extract the year-month format from the 'Custom field (Budget)' column in df2
+        pivot_df['month'] = pd.to_datetime(pivot_df['Sprint'])
+        # Now, merge the two datasets on the 'month' column
+        merged_df = pd.merge(pivot_df, fte_col, on='month', how='left')
+        print(merged_df)
+        # Rename the 'Dev' column to 'fte_contract'
+        merged_df.rename(columns={category: 'FTE Contract'}, inplace=True)
+        merged_df['OH'] = merged_df['FTE Contract'] - merged_df['Total Time Spent']
+        merged_df.drop(columns=['month'], inplace=True)
+        merged_df.rename(columns={'Sprint': 'Month'}, inplace=True)
         # Store the filtered DataFrame in the dictionary
-        category_dfs[category] = pivot_df
-        # Save the resulting DataFrame to an Excel file
-        pivot_df.to_excel(f'{category}pivoted_output.xlsx', index=False)
+        category_dfs[category] = merged_df
 
     return category_dfs
 
 
 def clean_string(input_string):
+    if input_string[:4] == "P000":
+        return "P0 - Vacation / Sickness"
+    elif input_string[:6] == "P999 -":
+        return "P999 - General2"
+    elif "P999 â" in input_string:
+        return "P999 - General4"
+    elif "P145-Corteva - IA" in input_string:
+        return "P145 - Corteva"
+    elif "P86 -Product" in input_string:
+        return "P86 - Product"
+    elif "P264 - Product-CP (Chempass)" == input_string:
+        return "P264 -Product CP"
+    elif "P84" in input_string:
+        return "p84-New program"
+    elif "P85 -Syngenta (Lavie)" == input_string:
+        return "P85-Syngenta"
+    elif "P192 - LAV 321 (Lavie)" == input_string:
+        return "P192-LAV 321"
     # Use regex to remove the content inside parentheses along with the space before it
     cleaned_string = re.sub(r'\s*\(.*?\)', '', input_string)
     return cleaned_string
 
 
-def formation_of_df_for_excel(df):
-    # Add new columns with default values
-    df['Entry Type'] = 'ACTUAL'
+def create_full_scale_for_excel(department, df):
+    # Get the current date and time
+    current_date = datetime.datetime.now()
+
+    # Extract the current month and year
+    current_month = current_date.month
+    current_year = current_date.year
+    if current_month == 1:
+        current_year -= 1
+
+    department_name = department
+    df['Entry Type'] = 'Actual'
     df['Employee ID'] = None
     df['Exp Type'] = 'Ongoing task'
     df['Jira name'] = None
     df['Employee Name'] = 'Total'
-    df['Approved by'] = None
-    df['New Project 1'] = None
-    df['New Project 2'] = None
+    df['Approved by'] = 'Ilia Zhidkov'
+    df['FTE left to Assign'] = 0
+    if department == "Bi":
+        department_name = "Bioinformation"
+        df['Department'] = "405 - Bioinformatics"
+        df['Role Ending'] = "T103 - Bioinformatician"
+    elif department == "Algo":
+        department_name = "Algo"
+        df['Department'] = "404 - Algorithm"
+        df['Role Ending'] = "T102 - Algorithm Developer"
+    elif department == "Dev":
+        department_name = "SoftwareDevelopment"
+        df['Department'] = "406 - Software Development"
+        df['Role Ending'] = "T112 - Software Developer"
+    else:
+        df['Department'] = f"{department}"
+        df['Role Ending'] = f"{department}"
 
-    # Reorganize the order of columns
-    cols = list(df.columns)
-    cols.insert(0, cols.pop(cols.index('Approved by')))
-    cols.insert(0, cols.pop(cols.index('Exp Type')))
-    cols.insert(0, cols.pop(cols.index('Employee Name')))
-    cols.insert(0, cols.pop(cols.index('Jira name')))
-    cols.insert(0, cols.pop(cols.index('Department')))
-    cols.insert(0, cols.pop(cols.index('Role Ending')))
-    cols.insert(0, cols.pop(cols.index('Exp Type')))
-    cols.insert(0, cols.pop(cols.index('Employee ID')))
-    cols.insert(0, cols.pop(cols.index('Entry Type')))
-    df = df[cols]
+    # fetching the template to work with in order to create the final report
+    template_path = os.path.join('..', 'config', 'template_for_CPBC.xlsx')
 
-    # Group the concatenated DataFrame by 'Department'
-    grouped_data = df.groupby('Department')
+    template_df = pd.read_excel(template_path, skiprows=1)
 
-    # Initialize a list to store the 6 DataFrames
-    grouped_dfs = []
+    columns = template_df.columns.tolist()
 
-    # Iterate over the groups and create a DataFrame for each group
-    for group_name, group_df in grouped_data:
-        if group_name != "Total":
-            # Append the DataFrame for the current group to the list
-            grouped_dfs.append([shorten_name(group_name), group_df])
+    # Iterate over each row of the original DataFrame
+    for index, row in df.iterrows():
+        # Initialize a dictionary to store values for each row
+        row_values = {}
 
-    return grouped_dfs
+        # Iterate over each column in the new DataFrame
+        for column in columns:
+            # Check if the column exists in the original DataFrame
+            if column in df.columns:
+                # If the column exists, assign the corresponding value from the original DataFrame
+                row_values[column] = row[column]
+            else:
+                # If the column doesn't exist, assign NaN
+                row_values[column] = np.nan
+
+        # Append the row values to the new DataFrame
+        template_df = template_df.append(row_values, ignore_index=True)
+
+        # Path to save the new Excel file
+        new_file_path = os.path.join('..', 'data', f'{department_name}_{current_month}_{str(current_year)[2:]}.xlsx')
+
+        # Copy the template to a new location
+        shutil.copy(template_path, new_file_path)
+
+        # Load the workbook and select the active sheet
+        wb = load_workbook(new_file_path)
+        ws = wb.active
+
+        # Write the DataFrame to the Excel sheet starting from the third row
+        for row_index, data_row in template_df.iterrows():
+            for col_index, value in enumerate(data_row):
+                ws.cell(row=row_index + 3, column=col_index + 1, value=value)
+
+        # Save the workbook
+        wb.save(new_file_path)
+
+    print(template_df)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
--- src/test.py
+++ src/test.py
@@ -4,4 +4,7 @@ import os
 
 input_path = os.path.join('..', 'data', 'jira_data.xlsx')
 
-CPBC_all.create_df_for_cpbc(input_path)
+dfs = CPBC_all.create_df_for_cpbc(input_path)
+
+for dept, df in dfs.items():
+    CPBC_all.create_full_scale_for_excel(dept, df, input_path)
